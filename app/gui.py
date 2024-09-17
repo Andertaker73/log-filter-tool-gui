690f300d0aad0b4813f52f403aefc50463b5b5f5
Anderson Veeck <anderson.veeck@compasso.com.br>
2024-09-17 17:42:28 -0300

    fixed process_filtered_log method input param filter issue

--- app/gui.py
+++ app/gui.py
@@ -214,15 +214,16 @@ class LogFilterApp(QMainWindow):
                                                                               encoding='utf-8') as out_file:
             capture_lines = False
             for line in log_origin:
-                if filter_param in line:
+                if filter_param in line:  # Captura todas as linhas que contêm o parâmetro de filtro
                     out_file.write(line)
-                    capture_lines = '*ERROR*' in line
+                    capture_lines = True  # Inicia a captura de linhas subsequentes
                 elif capture_lines:
-                    timestamp_match = re.match(r'\d{2}\.\d{2}\d{4} \d{2}:\d{2}:\d{2}\.\d{3}', line)
+                    # Verifica se a linha seguinte é parte do erro (ou seja, se não é um timestamp de nova entrada)
+                    timestamp_match = re.match(r'\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}:\d{2}\.\d{3}', line)
                     if not timestamp_match:
                         out_file.write(line)
                     else:
-                        capture_lines = False
+                        capture_lines = False  # Para de capturar quando encontrar um novo timestamp
 
         elapsed_time = time.time() - self.start_time
         formatted_time = format_time(elapsed_time)
